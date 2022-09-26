1548bd286b144a5da4aeebefc47b48a9f4ee2523
slowy07 <slowy.arfy@gmail.com>
2022-09-27 02:05:04 +0700

    feat: adding docker integration
    webui
    
    setup
    
    text2image
    
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="luna",
+    version="0.0.1",
+    description="Stable diffusion in tensorflow",
+    author="arfy slowy",
+    author_email="slowy.arfy@gmail.com",
+    platform=["any"],
+    url="https://github.com/slowy07/luna",
+    packages=find_packages(),
+)
