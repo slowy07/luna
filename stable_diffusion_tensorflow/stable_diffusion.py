570b1450f5872595f71f122ea6a2f93272ae58e5
slowy07 <slowy.arfy@gmail.com>
2022-11-12 06:10:14 +0700

    feat: update dataset
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- stable_diffusion_tensorflow/stable_diffusion.py
+++ stable_diffusion_tensorflow/stable_diffusion.py
@@ -258,20 +258,26 @@ def get_models(img_height, img_width, download_weights=True):
     encoder = Encoder()
     encoder = keras.models.Model(inp_img, encoder(inp_img))
 
-    text_encoder_weights_fpath = keras.utils.get_file(
-        origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/text_encoder.h5",
-        file_hash="d7805118aeb156fc1d39e38a9a082b05501e2af8c8fbdc1753c9cb85212d6619",
-    )
-    diffusion_model_weights_fpath = keras.utils.get_file(
-        origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/diffusion_model.h5",
-        file_hash="a5b2eea58365b18b40caee689a2e5d00f4c31dbcb4e1d58a9cf1071f55bbbd3a",
-    )
-    decoder_weights_fpath = keras.utils.get_file(
-        origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/decoder.h5",
-        file_hash="6d3c5ba91d5cc2b134da881aaa157b2d2adc648e5625560e3ed199561d0e39d5",
-    )
+    if download_weights:
+        text_encoder_weights_fpath = keras.utils.get_file(
+            origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/text_encoder.h5",
+            file_hash="d7805118aeb156fc1d39e38a9a082b05501e2af8c8fbdc1753c9cb85212d6619",
+        )
+        diffusion_model_weights_fpath = keras.utils.get_file(
+            origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/diffusion_model.h5",
+            file_hash="a5b2eea58365b18b40caee689a2e5d00f4c31dbcb4e1d58a9cf1071f55bbbd3a",
+        )
+        decoder_weights_fpath = keras.utils.get_file(
+            origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/decoder.h5",
+            file_hash="6d3c5ba91d5cc2b134da881aaa157b2d2adc648e5625560e3ed199561d0e39d5",
+        )
+        encoder_weights_fpath = keras.utils.get_file(
+            origin="https://huggingface.co/slowy07/stable_diffusion/resolve/main/encoder_newW.h5",
+            file_hash="56a2578423c640746c5e90c0a789b9b11481f47497f817e65b44a1a5538af754",
+        )
 
-    text_encoder.load_weights(text_encoder_weights_fpath)
-    diffusion_model.load_weights(diffusion_model_weights_fpath)
-    decoder.load_weights(decoder_weights_fpath)
-    return text_encoder, diffusion_model, decoder
+        text_encoder.load_weights(text_encoder_weights_fpath)
+        diffusion_model.load_weights(diffusion_model_weights_fpath)
+        decoder.load_weights(decoder_weights_fpath)
+        encoder.load_weights(encoder_weights_fpath)
+    return text_encoder, diffusion_model, decoder, encoder
